a455fd4c6ff615fe0934b1733eae8b7211e1e21a
Cjaay-r <chamodranaweera.64@gmail.com>
2026-08-19 14:58:06 +0530

    Implemented PatientService with mockito tests

---
+++ src/main/java/sunrisedentalsystem/service/PatientService.java
@@ -0,0 +1,14 @@
+package sunrisedentalsystem.service;
+
+import java.sql.SQLException;
+
+import sunrisedentalsystem.model.Patient;
+
+public interface PatientService {
+
+    Patient registerPatient(
+            Patient patient) throws SQLException;
+
+    Patient searchPatient(
+            int patientId) throws SQLException;
+}
