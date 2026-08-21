f63891488206613f5ceff853506f406b7418ab89
Cjaay-r <chamodranaweera.64@gmail.com>
2026-08-22 01:06:51 +0530

    Implemented dentist CRUD with database tests

--- src/main/java/sunrisedentalsystem/dao/DentistDAO.java
+++ src/main/java/sunrisedentalsystem/dao/DentistDAO.java
@@ -7,9 +7,18 @@ import sunrisedentalsystem.model.Dentist;
 
 public interface DentistDAO {
 
+    boolean addDentist(Dentist dentist)
+            throws SQLException;
+
     Dentist getDentistById(int dentistId)
             throws SQLException;
 
     List<Dentist> getAllDentists()
+            throws SQLException;
+
+    boolean updateDentist(Dentist dentist)
+            throws SQLException;
+
+    boolean deleteDentist(int dentistId)
             throws SQLException;
 }
